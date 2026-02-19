6585f5ee4c537c04ab53e9d2acdcbd9826fdf198
DishaAlagiya <dishaalagiya2994@gmail.com>
2026-02-19 22:49:36 +0530

    Remove duplicate root Django files

--- scrapper/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Link(models.Model):
-    def __str__(self):
-        return self.name or "Unnamed Link"
-    
-    address = models.CharField(max_length=1000, null= True, blank= True)
-    name = models.CharField(max_length=1000, null= True, blank= True)
